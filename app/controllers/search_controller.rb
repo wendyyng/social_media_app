e574c54dc8b9ff592433ae30c9131ef702a6e639
Wendy Ng <yng@ucalgary.ca>
2022-09-01 00:37:07 +0000

    Edit styling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,9 @@
 class SearchController < ApplicationController
   def index
-    if @keyword
-      @keyword = params[:q].values[0]
-    end
+
+    @keyword = params[:q].values[0]
     @query = Group.ransack(params[:q])
     @groups = @query.result(distinct: true)
   end
-
 
 end
